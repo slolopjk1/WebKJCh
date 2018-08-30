8fbad7ce0f3d3ba358cc534d84ad8cecef2baf70
parkgilljoo <slolopjk1@gmail.com>
2018-08-30 16:55:36 +0900

    copy script 2/2 완료

--- copyToDoList/main.js
+++ copyToDoList/main.js
@@ -17,7 +17,7 @@ inputText.onkeyup = function(event){
 var totalItems =0;
 
 function updateItemStatus(){
-    var chId = this.id.replace('cb_','');
+    var chId =  this.id.replace('cb_','');
     var itemText = document.getElementById('item_'+chId);
 
     if(this.checked){
@@ -28,21 +28,77 @@ function updateItemStatus(){
     }
 }
 
+function renameItem(){
+    var newText = prompt("what should this item be rename to?");
+    if(!newText || newText ==="" || newText ===" ") return false;
+
+    var spanId =this.id.replace('pencilIcon_','');
+    var span = document.getElementById('item_'+spanId);
+    span.innerText = newText;    
+}
+
+function removeItem(){
+
+    var listItemId = this.id.replace('li_','');
+    document.getElementById('li_' + listItemId).style.display = "none";
+ 
+}
+
+
 function addNewItem(list, itemText){
-    totalItems++;
+    // totalItems++;
+    var date = new Date();
+    var id = "" + date.getHours()+date.getMinutes()+date.getSeconds()+date.getMilliseconds();
 
     var listItem = document.createElement('li');
+    listItem.id ='li_'+id;
+    listItem.ondblclick = moveItem;
+
     var checkBox = document.createElement('input');
-    checkBox.type = 'checkBox';
-    checkBox.id='cb_' + totalItems;
+    checkBox.type = 'checkbox';
+    checkBox.id='cb_' + id;
     checkBox.onclick = updateItemStatus;
 
     var span = document.createElement('span');
-    span.id = 'item_' + totalItems;
+    span.id = 'item_' + id;
     span.innerText = itemText;
-    
+    // span.onclick = renameItem;
+
+    var pencilIcon = document.createElement('i');
+    pencilIcon.className='fas fa-pen';
+    pencilIcon.onclick = renameItem;
+
+    var minusIcon = document.createElement('i');
+    minusIcon.className ='fas fa-minus-square';
+    minusIcon.id ='minusIcon_'+id;
+    minusIcon.onclick= deleteItem;
+
+
+
     listItem.appendChild(checkBox);
-    listItem.innerText = itemText;
+    listItem.appendChild(span);
+    listItem.appendChild(pencilIcon);
+    listItem.appendChild(minusIcon);
     list.appendChild(listItem);
 
 }
+
+var donelist = document.getElementById('donelist');
+function moveItem(){
+    var listItemId = this.id.replace('li_', '');
+    var listItem = document.getElementById('li_'+listItemId);
+    var listItemParentId = listItem.parentElement;
+    console.log(listItemParentId);
+    if(listItemParentId == donelist){
+        todolist.appendChild(listItem);
+    }else{
+        donelist.appendChild(listItem);
+    }
+}
+
+function deleteItem(donelist){
+    var listItemId = this.id.replace('minusIcon_','');
+    document.getElementById('li_'+ listItemId).style.display="none";
+}
+
+
